4dcb4fa31ef29df32142b93870c3318f546d3e4c
markdzhurko <mark.dzhurko@gmail.com>
2022-10-25 16:31:40 +0300

    upgrade burger

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,15 @@
 const burger = document.querySelector('.header__burger')
 const burgerMenu = document.querySelector('.header__menu');
 const burgerLogin = document.querySelector('.header__login');
+const burgerClose = document.querySelector('.header__burger')
 
 burger.addEventListener('click', burgerToggle);
+burgerClose.addEventListener('click', burgerToggle);
 
 
 function burgerToggle() {
     burgerMenu.classList.toggle('show');
     burgerLogin.classList.toggle('show');
+    burgerClose.classList.toggle('show');
 }
+
